3933f38219836fc7ec2cc07d0494b134f1520baa
Diego <celtadiego@users.noreply.github.com>
2017-10-16 11:15:42 +0200

    Prueba git

---
+++ Module1.vb
@@ -0,0 +1,34 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim a, b, c, d, menor As Double
+        Console.WriteLine("Escriba el valor de a")
+        a = Convert.ToDouble(Console.ReadLine)
+
+        Console.WriteLine("Escriba el valor de b")
+        b = Convert.ToDouble(Console.ReadLine)
+
+        Console.WriteLine("Escriba el valor de c")
+        c = Convert.ToDouble(Console.ReadLine)
+
+        Console.WriteLine("Escriba el valor de d")
+        d = Convert.ToDouble(Console.ReadLine)
+
+        If a > b Then
+            menor = b
+        Else
+            menor = a
+        End If
+        If menor > c Then
+            menor = c
+        End If
+        If menor > d Then
+            menor = d
+        End If
+
+        Console.WriteLine("menor es: " & menor)
+        Console.ReadLine()
+    End Sub
+
+End Module
